dda516c214123bf3c4a2ad299e3d3dccffa3052d
unknown <kulesza.michal@gmail.com>
2020-09-18 13:26:29 +0200

    fix: z-index

--- src/components/navbar/styles/navbar.js
+++ src/components/navbar/styles/navbar.js
@@ -1,16 +1,20 @@
 import styled from "styled-components/macro";
 
 export const Main = styled.div`
+	position: relative;
 	max-width: 1920px;
 	margin: 0 auto;
 	padding-top: 20px;
 	width: 100%;
 	height: 5rem;
+	z-index: 1;
 `;
 
 export const Container = styled.div`
 	margin: 0 3.5rem;
 	height: 100%;
+	display: flex;
+	justify-content: space-between;
 `;
 
 export const Logo = styled.img`
