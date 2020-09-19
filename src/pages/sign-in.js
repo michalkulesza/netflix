dbd9b1ac39923d17d9851bb3ed9d2233f9c9d5e9
unknown <kulesza.michal@gmail.com>
2020-09-19 10:06:08 +0200

    add: signin page

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { HeaderContainer } from "../containers";
+import { HeaderContainer, FooterContainer } from "../containers";
 import Background from "../res/home-bg.jpg";
 
 export default function SignIn() {
-	return <HeaderContainer largeLogo={true} button={null} bg={Background}></HeaderContainer>;
+	return (
+		<>
+			<HeaderContainer largeLogo={true} button={null} bg={Background}>
+				//CREATE SIGNIN AND SIGNUP FORMS
+			</HeaderContainer>
+			<FooterContainer></FooterContainer>
+		</>
+	);
 }
