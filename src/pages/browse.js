c483f8c3a83082ef97000829e221ff4a4cb64b49
unknown <kulesza.michal@gmail.com>
2020-09-22 22:22:01 +0200

    improv: working on header

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Navbar, Header } from "../components/";
+import { NavbarContainer, HeaderContainer } from "../containers";
+import VideoFile from "../res/videos/mindhunter_trailer.mp4";
 
 const Browse = () => {
-	return <h1>Hello from browse</h1>;
+	return (
+		<>
+			<NavbarContainer>
+				<Navbar.Divide>
+					<Navbar.Logo smallLogo={true} />
+					<Navbar.Menu>
+						<Navbar.MenuItem>Home</Navbar.MenuItem>
+						<Navbar.MenuItem>Series</Navbar.MenuItem>
+						<Navbar.MenuItem>Films</Navbar.MenuItem>
+						<Navbar.MenuItem>Latest</Navbar.MenuItem>
+						<Navbar.MenuItem>My List</Navbar.MenuItem>
+					</Navbar.Menu>
+				</Navbar.Divide>
+			</NavbarContainer>
+			<HeaderContainer videoUrl={VideoFile}></HeaderContainer>
+		</>
+	);
 };
 
 export default Browse;
