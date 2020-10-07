21280df7d17bc907cc458b57de0d498a62d6d63c
unknown <kulesza.michal@gmail.com>
2020-10-07 23:16:34 +0200

    add: hook to handle calling fetch data for routes

--- src/hooks/use-fetch-init-data.js
+++ src/hooks/use-fetch-init-data.js
@@ -1,34 +1,96 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+	setInitialData,
+	fetchInitialDataBrowse,
+	fetchInitialDataSeries,
+	fetchInitialDataFilms,
+	fetchInitialDataLatest,
+} from "../redux/actions";
 
 const useFetchInitData = id => {
+	const dispatch = useDispatch();
 	const [prevRoute, setPrevRoute] = useState(null);
+	const cachedBrowseData = useSelector(state => state.cacheInitialData.browseCache);
+	const cachedSeriesData = useSelector(state => state.cacheInitialData.seriesCache);
+	const cachedFilmsData = useSelector(state => state.cacheInitialData.filmsCache);
+	const cachedLatestData = useSelector(state => state.cacheInitialData.latestCache);
+	const initialData = useSelector(state => state.initialData);
 
 	useEffect(() => {
 		if (prevRoute !== id.pathname) {
 			if (/^\/browse\/my-list/.test(id.pathname)) {
-				console.log("MY LIST");
+				console.log("Navigated to: /MY LIST");
 				return;
 			}
+
 			if (/^\/browse\/latest/.test(id.pathname)) {
-				console.log("LATEST");
+				if (!initialData) {
+					if (cachedLatestData) {
+						if (cachedLatestData !== initialData) {
+							dispatch(setInitialData(cachedLatestData));
+							console.log("Served cached latest data.");
+						}
+						return;
+					} else {
+						dispatch(fetchInitialDataLatest());
+						console.log("Fetched and cached series data.");
+					}
+				}
 				return;
 			}
+
 			if (/^\/browse\/films/.test(id.pathname)) {
-				console.log("FILMS");
+				if (!initialData) {
+					if (cachedFilmsData) {
+						if (cachedFilmsData !== initialData) {
+							dispatch(setInitialData(cachedFilmsData));
+							console.log("Served cached films data.");
+						}
+						return;
+					} else {
+						dispatch(fetchInitialDataFilms());
+						console.log("Fetched and cached series data.");
+					}
+				}
 				return;
 			}
+
 			if (/^\/browse\/series/.test(id.pathname)) {
-				console.log("SERIES");
+				if (!initialData) {
+					if (cachedSeriesData) {
+						if (cachedSeriesData !== initialData) {
+							dispatch(setInitialData(cachedSeriesData));
+							console.log("Served cached series data.");
+						}
+						return;
+					} else {
+						dispatch(fetchInitialDataSeries());
+						console.log("Fetched and cached series data.");
+					}
+				}
 				return;
 			}
+
 			if (/^\/browse/.test(id.pathname)) {
-				console.log("BROWSE");
+				if (!initialData) {
+					if (cachedBrowseData) {
+						if (cachedBrowseData !== initialData) {
+							dispatch(setInitialData(cachedBrowseData));
+							console.log("Served cached browse data.");
+						}
+						return;
+					} else {
+						dispatch(fetchInitialDataBrowse());
+						console.log("Fetched and cached browse data.");
+					}
+				}
 				return;
 			}
 
 			setPrevRoute(id.pathname);
 		}
-	}, [id, prevRoute]);
+	}, [id, prevRoute, cachedBrowseData, cachedSeriesData, cachedFilmsData, cachedLatestData, dispatch, initialData]);
 	return null;
 };
 
