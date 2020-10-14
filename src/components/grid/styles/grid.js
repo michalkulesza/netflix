ea01d9bd9857e4535bdd343374c4135a4a4bcccb
unknown <kulesza.michal@gmail.com>
2020-10-14 18:15:24 +0200

    add: breakpoints

--- src/components/grid/styles/grid.js
+++ src/components/grid/styles/grid.js
@@ -1,4 +1,5 @@
 import styled from "styled-components/macro";
+import { respondTo } from "../../../styles/respondTo";
 
 export const Wrapper = styled.div`
 	width: 100%;
@@ -10,4 +11,35 @@ export const Main = styled.div`
 	width: 100%;
 	display: grid;
 	grid-template-columns: repeat(4, 1fr);
+
+	${respondTo.xs`
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1em;
+	`}
+
+	${respondTo.sm`
+		grid-template-columns: repeat(3, 1fr);
+		gap: 1em;
+	`}
+
+	${respondTo.md`
+		grid-template-columns: repeat(4, 1fr);
+		gap: 1em;
+	`}
+
+	${respondTo.lg`
+		grid-template-columns: repeat(5, 1fr);
+		gap: 1em;
+	`}
+
+	${respondTo.xl`
+		grid-template-columns: repeat(6, 1fr);
+		gap: 1em;
+	`}
+	
+
+	${respondTo.xxl`
+		grid-template-columns: repeat(8, 1fr);
+		gap: 1em;
+	`}
 `;
