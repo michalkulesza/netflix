79fe3d539a56016a3312bfd3806cf06f1e35ed34
Michal <kulesza.michal@gmail.com>
2020-10-23 16:33:58 +0200

    bugfix: first item was not given correct trans origin

--- src/helpers/markItemsPosition.js
+++ src/helpers/markItemsPosition.js
@@ -1,19 +1,17 @@
 export const markItemsPosition = (i, isFirstSlide, totalTilesInVievport) => {
-	if (i && isFirstSlide && totalTilesInVievport) {
-		const offset = isFirstSlide ? 0 : 1;
-		const items = totalTilesInVievport - 2;
+	const offset = isFirstSlide ? 0 : 1;
+	const items = totalTilesInVievport - 2;
 
-		if (i === 0 + offset) {
-			return "first";
-		}
+	if (i === 0 + offset) {
+		return "first";
+	}
 
-		if (i > 0 + offset && i < items + offset) {
-			return "middle";
-		}
+	if (i > 0 + offset && i < items + offset) {
+		return "middle";
+	}
 
-		if (i === items + offset) {
-			return "last";
-		}
+	if (i === items + offset) {
+		return "last";
 	}
 
 	return "outside";
