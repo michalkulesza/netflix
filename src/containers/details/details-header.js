41689155c148d06199b1d47df30e1051d0c2a9ca
Michal <kulesza.michal@gmail.com>
2020-10-29 11:38:36 +0100

    add: like, dislike and add to list in details

--- src/containers/details/details-header.js
+++ src/containers/details/details-header.js
@@ -2,10 +2,11 @@ import React, { useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Button } from "../../components";
 import { setGlobalMute, setIsDetails } from "../../redux/actions/toggles";
+import { dislikeVideo, likeVideo, toggleVideoToList } from "../../redux/actions/user";
 
 import { GrPlayFill, GrClose } from "react-icons/gr";
 import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
-import { BiPlus, BiLike, BiDislike } from "react-icons/bi";
+import { BiPlus, BiMinus, BiLike, BiDislike } from "react-icons/bi";
 import placeholder from "../../res/images/placeholder_w.jpg";
 import { useEffect } from "react";
 
@@ -18,6 +19,8 @@ const DetailsHeaderContainer = ({ item, scrolled }) => {
 	const [isPlaceholder, setIsPlaceholder] = useState(true);
 	const globalMute = useSelector(state => state.toggles.globalMute);
 	const headerData = useSelector(state => state.misc.headerVideo);
+	const userID = JSON.parse(localStorage.getItem("authUser"))?.uid;
+	const { liked, disliked, list } = useSelector(state => state.user);
 
 	useEffect(() => {
 		if (VideoPlayer.current) {
@@ -34,22 +37,20 @@ const DetailsHeaderContainer = ({ item, scrolled }) => {
 			}
 		}
 	}, [scrolled]);
-
-	const handleClose = () => {
-		dispatch(setIsDetails(false));
-	};
-
-	const handleMute = () => {
-		dispatch(setGlobalMute(!globalMute));
-	};
+	console.log(item.id);
+	const handleClose = () => dispatch(setIsDetails(false));
+	const handleMute = () => dispatch(setGlobalMute(!globalMute));
+	const handleListClick = () => dispatch(toggleVideoToList(userID, item.details[0].id));
+	const handleLikeClick = () => dispatch(likeVideo(userID, item.details[0].id));
+	const handleDislikeClick = () => dispatch(dislikeVideo(userID, item.details[0].id));
 
 	return (
 		<DetailsHeader>
-			{headerData && item ? (
+			{headerData && item.details[0] ? (
 				<>
 					<DetailsHeader.Video src={headerData.src} muted={globalMute} ref={VideoPlayer} />
 					<DetailsHeader.Cover
-						src={item.details.backdrop_path_1280 ? item.details.backdrop_path_1280 : placeholder}
+						src={item.details[0].backdrop_path_1280 ? item.details[0].backdrop_path_1280 : placeholder}
 						isPlaceholder={isPlaceholder}
 					/>
 					<DetailsHeader.Overlay>
@@ -59,13 +60,24 @@ const DetailsHeaderContainer = ({ item, scrolled }) => {
 								<Button.Square>
 									<GrPlayFill /> Play
 								</Button.Square>
-								<Button.Round label="Add to My List">
-									<BiPlus />
+								<Button.Round
+									label={list.includes(item.details[0].id) ? "Remove from My List" : "Add to My List"}
+									onMouseDown={handleListClick}
+								>
+									{list.includes(item.details[0].id) ? <BiMinus /> : <BiPlus />}
 								</Button.Round>
-								<Button.Round label="I like this">
+								<Button.Round
+									inverted={liked.includes(item.details[0].id)}
+									label={liked.includes(item.details[0].id) ? "Remove like" : "I like this"}
+									onMouseDown={handleLikeClick}
+								>
 									<BiLike />
 								</Button.Round>
-								<Button.Round label="Not for me">
+								<Button.Round
+									inverted={disliked.includes(item.details[0].id)}
+									label={disliked.includes(item.details[0].id) ? "Remove dislike" : "Not for me"}
+									onMouseDown={handleDislikeClick}
+								>
 									<BiDislike />
 								</Button.Round>
 							</DetailsHeader.ButtonsContainer>
